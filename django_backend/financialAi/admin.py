f3cc16a116f5cf3f315b58c24710fedba5cbb010
sahil190603 <113009082+sahil190603@users.noreply.github.com>
2025-03-05 11:16:58 +0530

    Delete django_backend directory

--- django_backend/financialAi/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import FinancialAI, MarketOccupancy
-
-# Register your models here.
-admin.site.register(FinancialAI)
-admin.site.register(MarketOccupancy)
